fb8ece353b4b0a92a8a6341bfdef04c09671517b
Adarsh2709 <jhaadarsh709@gmail.com>
2026-09-13 18:37:41 +0530

    Update deploy script for Anon Aadhaar constructor args

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -7,9 +7,12 @@ import "../contracts/ChaupalMembership.sol";
 contract DeployChaupal is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        address verifierAddress = vm.envOr("VERIFIER_ADDRESS", address(0));
+        uint256 appSeed = vm.envOr("APP_NULLIFIER_SEED", uint256(123456789));
+
         vm.startBroadcast(deployerPrivateKey);
 
-        ChaupalMembership chaupal = new ChaupalMembership();
+        ChaupalMembership chaupal = new ChaupalMembership(verifierAddress, appSeed);
         console.log("ChaupalMembership deployed to:", address(chaupal));
 
         vm.stopBroadcast();
